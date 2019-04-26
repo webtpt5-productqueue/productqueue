4b9e50f4a6c1b1b9c2e7e54855fbf636df2c8aff
Jonathan Daly <dalyj138@gmail.com>
2019-04-25 22:55:48 -0400

    Setup redux store.

--- product-queue/src/index.js
+++ product-queue/src/index.js
@@ -3,5 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
