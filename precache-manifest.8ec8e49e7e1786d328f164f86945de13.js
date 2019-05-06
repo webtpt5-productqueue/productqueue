39de23bf1afbe848b9f5b5ef9c35522b21020560
Cedricium <amaya.cedric@gmail.com>
2019-05-05 18:49:31 -0700

    Updates

---
+++ precache-manifest.8ec8e49e7e1786d328f164f86945de13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc2d4afaa290bfc53094cb0db66ea40",
+    "url": "/productqueue/index.html"
+  },
+  {
+    "revision": "52ed7e5a7120aec55bdf",
+    "url": "/productqueue/static/css/main.41b38ce4.chunk.css"
+  },
+  {
+    "revision": "858136b630c79e6b7706",
+    "url": "/productqueue/static/js/2.0a13264a.chunk.js"
+  },
+  {
+    "revision": "52ed7e5a7120aec55bdf",
+    "url": "/productqueue/static/js/main.681cd563.chunk.js"
+  },
+  {
+    "revision": "b7b8d7e614380fca0941",
+    "url": "/productqueue/static/js/runtime~main.a7d8e924.js"
+  }
+]);
